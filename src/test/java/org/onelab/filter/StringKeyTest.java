4bee6294385d07fc0606da9705f99080e745793d
Paolo Castagna <pc@talisplatform.com>
2010-08-12 14:07:18 +0000

    PLAT-1117 - Improving coverage...

--- src/test/java/org/onelab/filter/StringKeyTest.java
+++ src/test/java/org/onelab/filter/StringKeyTest.java
@@ -1,7 +1,27 @@
 package org.onelab.filter;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
 
 public class StringKeyTest {
 
+	@Test
+	public void testStringKeyConstructor() throws UnsupportedEncodingException {
+		String str = "toto";
+		byte[] bytes1 = str.getBytes();
+		byte[] bytes2 = str.getBytes("UTF-8");
+		StringKey key = new StringKey(str);
 
+		assertNotNull(key);
+		assertFalse(new String(bytes1).equals(new String(key.getBytes())));
+		assertEquals(new String(bytes2), new String(key.getBytes()));
+		assertEquals(1.0, key.getWeight(), 0.01);
+	}
+	
 }
