5642ebe0244d346989685dafa96f8e085f1303f2
Paolo Castagna <pc@talisplatform.com>
2010-08-12 07:56:24 +0000

    PLAT-1117 - Moved TestFilter from org.onelab.test to org.onelab.filter.

--- src/test/java/org/onelab/filter/TestFilter.java
+++ src/test/java/org/onelab/filter/TestFilter.java
@@ -46,7 +46,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.onelab.test;
+package org.onelab.filter;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
