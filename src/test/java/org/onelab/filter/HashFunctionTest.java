2f6ff00830bb44c2d98d9d2979769ac51cd1e9c9
Paolo Castagna <pc@talisplatform.com>
2010-08-13 10:12:24 +0000

    PLAT-1117 - more tests.

--- src/test/java/org/onelab/filter/HashFunctionTest.java
+++ src/test/java/org/onelab/filter/HashFunctionTest.java
@@ -1,6 +1,7 @@
 package org.onelab.filter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.UnsupportedEncodingException;
 
